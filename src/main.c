05b7c10b2fb92b95c4c4f89d88f48b6064885081
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2021-08-08 23:34:06 -0400

    Updates

--- src/main.c
+++ src/main.c
@@ -30,7 +30,7 @@ int main(void) {
 
     char string[100];
 
-    char winPath[] = "C:/Users/lajua/Desktop/Programming/Chip8/src/roms/spockpaperscissors.ch8";
+    char winPath[] = "C:/Users/lajua/Desktop/Programming/Chip8/src/roms/tombstontipp.ch8";
     char macPath[] = "/Users/lajuanstation/Desktop/Chip8C/src/roms/RPS.ch8";
     char archPath[] = "/home/lajuan/Programming/Chip8C/roms/Fishie.ch8";
 
