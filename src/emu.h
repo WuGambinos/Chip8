e218ae33b94410710fbc6e066e489753cc7832a3
WuGambinos <69660261+WuGambinos@users.noreply.github.com>
2021-09-13 19:55:56 -0400

    Updating documentation for emu.h

--- src/emu.h
+++ src/emu.h
@@ -44,21 +44,100 @@ typedef struct Chip8
     //General Purpose Registers V0 - VF
     uint8_t V[16];
 
+    //Halt flag
     uint8_t halt;
 
+    //Draw Flag
     uint8_t drawFlag;
 
 } Chip8;
 
 //Function Declarations
+
+/*
+ * Function: loadProgram
+ * -------------------
+ * Loads rom into a buffer then loads rom from buffer 
+ * into Chip8 memory starting at address 0x200
+ * 
+ * state: pointer to Chip8 struct
+ * f: File Object created using rom file
+ * fsize: size of the file
+ */
 void loadProgram(Chip8* state, FILE *f, int fsize);
+
+
+/*
+ * Function: InitChip8
+ * -------------------
+ * Initializes the Chip8 System
+ * 
+ * returns: pointer to Chip8 Struct
+ */
 Chip8* InitChip8(void);
+
+
+/*
+ * Function: emulateCycle
+ * --------------------------------
+ * Executes an Instruction
+ * 
+ * s: pointer to Chip8 struct
+ */
 void emulateCycle(Chip8* s);
+
+/*
+ * Function: DisassembleChip80p
+ * ----------------------------
+ * Disassembles opcode for rom of Chip8 System
+ * codebuffer: buffer that holds the opcodes for the given rom
+ * pc: Program Counter
+ */
 void DisassembleChip8Op(uint8_t *codebuffer, int pc);
+
+
+/*
+ * Function: printRegisters
+ * ------------------------
+ * Prints values of registers in Chip8 system to the console
+ * 
+ * s: pointer to Chip8 struct
+ */
 void printRegisters(Chip8* s);
+
+
+/*
+ * Function: printTimers
+ * ---------------------
+ * Prints values of delay and sound timers of Chip8 system to the console
+ * 
+ * s: pointer to Chip8 struct
+ */
 void printTimers(Chip8* s);
+
+
+/*
+ * Function: printStack
+ * --------------------
+ * Prints values on the stack of the Chip8 system to the console
+ * 
+ * s: pointer to Chip8 struct
+ */
 void printStack(Chip8* s);
+
+
+/*
+ * Function: printState
+ * --------------------
+ * Prints values of delay and sound timers of Chip8 system to the console
+ * Prints values on the stack of the Chip8 system to the console
+ * Prints values of registers in the Chip8 system to the console
+ * 
+ * s: pointer to Chip8 struct
+ */
 void printState(Chip8* s);
+
+
 void drawGraphics(Chip8* s);
 void checkKeys(Chip8 *s);
 
